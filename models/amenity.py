1dae39903c317a07c41af537263f021140bb6dc2
nayere <jobdavy64@outlook.com>
2024-02-11 15:54:40 -0800

    anemities

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,6 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """The class for managing amenity objects"""
+    """Class for managing amenity objects"""
 
     name = ""
