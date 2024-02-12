3c1f06b8802ac1a2d9ca247992b275a4aca2604e
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-02-12 03:10:42 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""This module creates a Amenity class"""
+"""Module creates a Amenity class"""
 
 from models.base_model import BaseModel
-
 
 class Amenity(BaseModel):
     """Class for managing amenity objects"""
